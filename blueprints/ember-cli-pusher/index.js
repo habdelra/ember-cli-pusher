3d8f6a462a030dd6a370c8476a79295ac90c2815
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2015-07-15 15:06:32 -0400

    updated bower location

--- blueprints/ember-cli-pusher/index.js
+++ blueprints/ember-cli-pusher/index.js
@@ -3,6 +3,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('ember-pusher', 'habdelra/ember-pusher-component');
+    return this.addBowerPackageToProject('ember-pusher', 'https://github.com/habdelra/ember-pusher-component.git#~0.1.1');
   }
 };
